c7ce85fc5bdd63ab7b30a3db73dc732efefcd1e6
Ornly1 <gnottz10@gmail.com>
2021-05-03 06:18:45 +0700

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <>
-      
+      <Navbar />
     </>
   );
 }
